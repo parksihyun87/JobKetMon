1888ecba9bea5a9d249b6652b492fedf2e0b7757
박시현 <solarhyun87@gmail.com>
2025-05-25 05:51:32 +0900

    잡켓몬 정리

--- src/StrangerField.java
+++ src/StrangerField.java
@@ -27,7 +27,7 @@ public class StrangerField implements Field{
         if(mBattle.fight(elecList,mJobKiMonBox)){
             System.out.println("'잘 도전하였다. 나는 인생들의 원류 그 자체이다. 인생은 계속 생겨나고, 그안의 생동성들은 도전하도록 만들겠지.");
             System.out.println("태어날때부터 고정된 길은 없으나, 발걸음이 헤이하면 의미없는 모래로 풀어져 돌아가게 된다.");
-            System.out.println("길을 만들어가려는 자의 종국에 닥칠일도 일반이나, 최후에 누여질 장소의 의미가 다르리라.");
+            System.out.println("길을 만드려고 심장을 듣는 자의 종국에 닥칠일도 일반이나, 최후에 누여질 장소의 의미가 다르리라.");
             System.out.println("비록 시간속에서 많은 것들이 퇴색하겠으나 사람들안에서만 전해지는 '의미'라는 것들은 인생의 바다안에 영원히 존재할 것이다.'");
             System.out.println("낯선 사람이 가벼운 발걸음으로 떠났습니다. 승리");
         } else {
