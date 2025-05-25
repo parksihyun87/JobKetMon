f06c4314d71b5d6803e61cdef336eb3d43b6d059
박시현 <solarhyun87@gmail.com>
2025-05-26 00:47:22 +0900

    잡켓몬 정리

--- src/ElectricField.java
+++ src/ElectricField.java
@@ -31,17 +31,45 @@ public class ElectricField implements Field{
         } else {
             System.out.println("패배");
         }
-        System.out.println("망토를 뒤집어 쓴 수상한 사람이 나타났다.");
-        System.out.println("'과연 끝이란 있는가, 모두 영겁의 되풀이인 것을. 성취란 있는가, 금세 공허해질 것을.");
-        System.out.println(" 친밀함 이란 있는가, 눈만 감아도 마음은 천리길 밖인 것을. 그대, 없어질 무엇을 이룩키 위하여 끝없이 쌓아올리려 하는가.");
-        System.out.println("너의 존재는 우주 가운데 한 낱 모래알, 까마득히 많은 새떼 무리의 일개 구성원일뿐이지만, 이곳에 너의 심장이 뛰고 있음을 증명하여 보여라.'");
-        System.out.println("수상한 남자에게 도전하시겠습니까?(y/n)");
-        Scanner input = new Scanner(System.in);
-        String confirm = input.nextLine();
-        if(confirm.equals("y")){
-            System.out.println("전투가 시작됩니다.");
-            stranger.introduce();
-            stranger.callStrangerField(mJobKiMonBox);
+        while(true) {
+            boolean qtrue= false;
+            System.out.println(" 망토를 뒤집어 쓴 수상한 사람이 중얼거리며 나타났다.");
+            System.out.println("'과연 끝이란 있는가, 모두 영겁의 되풀이인 것을. 성취란 있는가, 금세 공허해질 것을.");
+            System.out.println(" 친밀함 이란 있는가, 눈만 감아도 마음은 천리길 밖인 것을.");
+            System.out.println("수상한 남자에게 도전하시겠습니까?(y/n)");
+            Scanner input = new Scanner(System.in);
+            String confirm = input.nextLine();
+            if (confirm.equals("y")) {
+                System.out.println("나와 대결하려면 3가지 질문에 대답해야 한다.");
+                System.out.println("1. 지금 위치에서 북쪽으로 5km, 동쪽으로 10km, 서쪽으로 5km, 남쪽으로 5km를 이동해도 나는 제자리이다. 나의 원래 위치는 어디인가");
+                String answer1 = input.nextLine();
+                if("남극".equals(answer1)) {
+                    System.out.println("2. A은(는) 남쪽에 있지만 A은(는) 남쪽에 없다. A는 무엇인가(띄어쓰기 입력 필요)");
+                    String answer2 = input.nextLine();
+                    if("지구의 반".equals(answer2)||"지구의 절반".equals(answer2)||"세계의 반".equals(answer2) ||"세계의 절반".equals(answer2)) {
+                        System.out.println("3. 정오에 시에네의 땅에 막대기를 수직으로 세우면 그림자가 없다. 같은 시각 알렉산드리아의 땅에 막대기를 세우면 그림자 끝과 7.2도이다. 그 둘 사이 거리가 5000스타디아일때");
+                        System.out.println("지구의 둘레는 몇 스타디아인가?(두 도시의 경도가 같고, 지구가 완전한 구형이라고 가정한다.)");
+                        Integer answer3 =0;
+                        try {
+                            answer3 = input.nextInt();
+                            input.nextLine();
+                        } catch (Exception e) {
+                            System.out.println("숫자를 정확히 입력하세요.");
+                            input.nextLine();
+                            continue;
+                        }
+                        if(answer3==250000) {
+                            System.out.println("모두 정답이다.");
+                            System.out.println("전투가 시작됩니다.");
+                            stranger.introduce();
+                            stranger.callStrangerField(mJobKiMonBox);
+                            break;
+                        }
+                    }
+                }
+
+            }
+            System.out.println("답이 틀려 처음으로 돌아갑니다.");
         }
     }//콜 파이어필드 끝
 }//파필 클래스 끝
--- src/StrangerField.java
+++ src/StrangerField.java
@@ -12,7 +12,7 @@ public class StrangerField implements Field{
     }
 
     public StrangerField(){
-        elecList.add(new electricMon("큰부리꽁지",200,"꽁지꽁지","몸통박치기",20,80,"번개멍때리기",0,80));
+        elecList.add(new electricMon("큰부리꽁지",150,"꽁지꽁지","몸통박치기",20,80,"번개멍때리기",0,80));
     }
 
     public void callStrangerField(ArrayList<PMAction> mJobKiMonBox){
@@ -25,11 +25,9 @@ public class StrangerField implements Field{
         System.out.println("전투가 시작됩니다.");
 
         if(mBattle.fight(elecList,mJobKiMonBox)){
-            System.out.println("'잘 도전하였다. 나는 인생들의 원류 그 자체이다. 인생은 계속 생겨나고, 그안의 생동성들은 도전하도록 만들겠지.");
-            System.out.println("태어날때부터 고정된 길은 없으나, 발걸음이 헤이하면 의미없는 모래로 풀어져 돌아가게 된다.");
-            System.out.println("길을 만드려고 심장을 듣는 자의 종국에 닥칠일도 일반이나, 최후에 누여질 장소의 의미가 다르리라.");
-            System.out.println("비록 시간속에서 많은 것들이 퇴색하겠으나 사람들안에서 전해지는 '의미'라는 것들은 인생의 바다안에 영원히 존재할 것이다.'");
-            System.out.println("낯선 사람이 가벼운 발걸음으로 떠났습니다. 승리");
+            System.out.println("'주절거림은 주절거림으로,,,그래도 인생에 있어 의미정도는 담아도 되겠지");
+            System.out.println("좋은 승부였다.'");
+            System.out.println("낯선 사람이 엔딩 배지를 주었습니다. 승리");
         } else {
             System.out.println("패배");
         }
